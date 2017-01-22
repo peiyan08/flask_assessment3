46e32decef2eefbcaa212a475654b4e9cc445ae4
vagrant <vagrant@vagrant.vm>
2017-01-22 00:01:16 +0000

    finish part1

---
+++ application.py
@@ -0,0 +1,8 @@
+
+class Applicants(object):
+
+    def __init__(self, fname, lname, salary, job):
+        self.fname = fname
+        self.lname = lname
+        self.salary = salary
+        self.job = job
---
+++ server.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from application import Applicants
+
+app = Flask(__name__)
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+
+@app.route("/")
+def home():
+    """Return Homepage"""
+
+    return render_template('index.html')
+
+@app.route("/application-form")
+def application_form():
+    """Return Application Form Page"""
+
+    return render_template('application-form.html')
+
+@app.route("/application-success", methods=["POST"])
+def application_succss():
+    fname = request.form.get("fname")
+    lname = request.form.get("lname")
+    salary = float(request.form.get("salary"))
+    job = request.form.get("jobtype")
+    new_applicant = Applicants(fname, lname, salary, job)
+
+    return render_template("application-response.html", new_applicant=new_applicant)
+    
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
